952742a4e9c6b7fbc8367a6e0cae5bc0492b234b
MRomina <mrominapereyra@outlook.com>
2023-07-10 13:46:33 -0300

    mejore los estilos y agregue footer

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm navbar-light bg-light">
+document.getElementById("header").innerHTML=`<nav class="navbar navbar-expand-sm  navbar-light bg-light" >
 <div class="container">
-  <a class="navbar-brand" href="index.html">Menú</a>
+  <a class="navbar-brand" href="index.html">Turismo x el mundo</a>
   <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
       aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -23,4 +23,126 @@ document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-s
   </div>
 </div>
 </nav>
+
 `
+document.getElementById("footer").innerHTML=`<footer class="text-center text-white" style="background-color: #f0f3f5; margin-top: 20px;">
+<!-- Grid container -->
+<div class="container p-4">
+  <!-- Section: Images -->
+  <section class="">
+    <div class="row">
+      <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+        <div
+          class="bg-image hover-overlay ripple shadow-1-strong rounded"
+          data-ripple-color="light"
+        >
+          <img
+            src="https://mdbcdn.b-cdn.net/img/new/fluid/city/113.webp"
+            class="w-100"
+          />
+          <a href="#!">
+            <div
+              class="mask"
+              style="background-color: rgba(251, 251, 251, 0.2);"
+            ></div>
+          </a>
+        </div>
+      </div>
+      <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+        <div
+          class="bg-image hover-overlay ripple shadow-1-strong rounded"
+          data-ripple-color="light"
+        >
+          <img
+            src="https://mdbcdn.b-cdn.net/img/new/fluid/city/111.webp"
+            class="w-100"
+          />
+          <a href="#!">
+            <div
+              class="mask"
+              style="background-color: rgba(251, 251, 251, 0.2);"
+            ></div>
+          </a>
+        </div>
+      </div>
+      <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+        <div
+          class="bg-image hover-overlay ripple shadow-1-strong rounded"
+          data-ripple-color="light"
+        >
+          <img
+            src="https://mdbcdn.b-cdn.net/img/new/fluid/city/112.webp"
+            class="w-100"
+          />
+          <a href="#!">
+            <div
+              class="mask"
+              style="background-color: rgba(251, 251, 251, 0.2);"
+            ></div>
+          </a>
+        </div>
+      </div>
+      <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+        <div
+          class="bg-image hover-overlay ripple shadow-1-strong rounded"
+          data-ripple-color="light"
+        >
+          <img
+            src="https://mdbcdn.b-cdn.net/img/new/fluid/city/114.webp"
+            class="w-100"
+          />
+          <a href="#!">
+            <div
+              class="mask"
+              style="background-color: rgba(251, 251, 251, 0.2);"
+            ></div>
+          </a>
+        </div>
+      </div>
+      <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+        <div
+          class="bg-image hover-overlay ripple shadow-1-strong rounded"
+          data-ripple-color="light"
+        >
+          <img
+            src="https://mdbcdn.b-cdn.net/img/new/fluid/city/115.webp"
+            class="w-100"
+          />
+          <a href="#!">
+            <div
+              class="mask"
+              style="background-color: rgba(251, 251, 251, 0.2);"
+            ></div>
+          </a>
+        </div>
+      </div>
+      <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+        <div
+          class="bg-image hover-overlay ripple shadow-1-strong rounded"
+          data-ripple-color="light"
+        >
+          <img
+            src="https://mdbcdn.b-cdn.net/img/new/fluid/city/116.webp"
+            class="w-100"
+          />
+          <a href="#!">
+            <div
+              class="mask"
+              style="background-color: rgba(251, 251, 251, 0.2);"
+            ></div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Section: Images -->
+</div>
+<!-- Grid container -->
+
+<!-- Copyright -->
+<div class="text-center p-3" style="background-color: #1a202c;">
+  © 2023 Copyright:
+  <a class="text-white" href="https://mromina85.github.io/23020tpo-turismoxelmundo/index.html">turismoxelmundo.com.ar</a>
+</div>
+<!-- Copyright -->
+</footer>`
